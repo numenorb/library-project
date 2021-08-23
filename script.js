6d3459b209733abd5dfdc7c9ec431b9ce3c582ff
Matthew Breig <matthew.breig@gmail.com>
2021-08-23 14:59:31 -0400

    Got the table to work with read/unread toggle. Fixed the delete button being added outside of the table.

--- script.js
+++ script.js
@@ -5,11 +5,13 @@ let pageCount = document.querySelector("#page-count");
 let readStatus = document.querySelector("#read-status");
 let submitButton = document.querySelector("#submit-button");
 let allForms = document.querySelectorAll(".display-form");
+let bookForm = document.querySelector("#book-form");
 let libraryDisplay = document.querySelector("#library-table");
 let libraryHeaders = document.querySelector("#table-headers");
+let newBookButton = document.querySelector("#new-book");
 
 submitButton.addEventListener('click', () => {addBookToLibrary();});
-
+newBookButton.addEventListener('click', () => {toggleBookForm();});
 
 function Book(title, author, pageCount, read) {
     this.title = title;
@@ -38,6 +40,7 @@ function addBookToLibrary(){
     }
     myLibrary.unshift(newBook);  
     updateDisplay(myLibrary);
+    toggleBookForm();
     clearForm();
 }
 
@@ -51,22 +54,36 @@ function clearForm(){
 function createTableRow(title, author, pageCount, read, deleteStatus){
     const tableRow = document.createElement('tr');
     let deleteButton;
-    for(i = 0; i < arguments.length - 1; i++){
+    let readButton;
+    for(i = 0; i < arguments.length - 2; i++){
         let tableItem = document.createElement('th');
         tableItem.innerText = arguments[i];
         tableRow.appendChild(tableItem);
     }
+    let tableButton = document.createElement('th');
+    
+    if (read === true){
+        readButton = document.createElement('input');
+        readButton.setAttribute('type', 'checkbox');
+        tableButton.append(readButton);
+        readButton.checked = true;
+    } else if (read === false){
+        readButton.checked = false
+    } else {
+        tableButton.innerText = "Read?";
+    }
+    tableRow.append(tableButton)
+    let tableButton2 = document.createElement('th');
     if (deleteStatus === true){
         deleteButton = document.createElement('button');
-        deleteButton.setAttribute('data-delete','');
+        tableButton2.appendChild(deleteButton);
+        deleteButton.innerText = 'Delete';
     } else{
-        deleteButton = document.createElement('th');
+        tableButton2.innerText = 'Delete?';
     }
-    tableRow.appendChild(deleteButton);
-    deleteButton.innerText = 'Delete';
+    tableRow.appendChild(tableButton2);
     libraryDisplay.appendChild(tableRow);
 }
-// 8.22 - need to fix this. Need to reach readStatus.checked 
 
 
 function updateDisplay(bookList){
@@ -76,19 +93,17 @@ function updateDisplay(bookList){
     createTableRow("Title", "Author", "Pages", "Read", false)
     for (let i = 0; i < bookList.length; i++){
         let readTheBook = (bookList[i].read ? "Read" : "Unread")
-        createTableRow(bookList[i].title, bookList[i].author, bookList[i].pageCount, readTheBook, true);
+        createTableRow(bookList[i].title, bookList[i].author, bookList[i].pageCount, bookList[i].read, true);
     }
+}
+
+function toggleBookForm(){
+    bookForm.style.display = (bookForm.style.display === 'none' ? 'flex' : 'none');
 }
 
 
 
-
-
 /**
- * All books are going to be scored into the myLibrary array
- * addBookToLibrary() should loop through the array and display each book on the page.
- * *** This could be in some sort of table, or each on their own 'card'
- * *** Manually add a few to begin to get the layout/display
  * A 'New Book' button that brings up a form allowing user to input the details of the book: author, title, # pages, read or not?
  * include a 'remove' button on each book's display -- HINT: need to associate your DOM element with the actual book objects: data attribute that corresponds to the index of the library array
  * include a 'read' button that will toggle the read status 
